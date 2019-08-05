09e3a52d2943772ceedccd11ae6f3e493031e15f
kerry okpere <kerryokpere@gmail.com>
2019-08-05 12:39:32 +0100

    Feat: establish connection with Database
    Create Table in the Database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
+import { UserModel } from "./api/User";
 import app from "./app";
+import db from "./shared/database";
 import { logger } from "./utils/logger";
 
 const port =  3000;
 app.set("port", port);
+
+db.connect((err) => {
+    if (err) {
+        logger.error(err);
+        throw new Error(err.message);
+    }
+    logger.info("Connected to database successfully");
+    new UserModel().init();
+});
+
 app.listen(app.get("port"), (err) => {
     if (err) {
         return logger.error(err);
