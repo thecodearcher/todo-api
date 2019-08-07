1f2c50d7eabe68b2f8e3d46eac6e14ca1edc6585
kerry okpere <kerryokpere@gmail.com>
2019-08-07 09:42:15 +0100

    Update: Change Database password

--- src/shared/database.ts
+++ src/shared/database.ts
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host: "localhost",
         user: "kerry",
-        password: "flexible",
+        password: "password",
         database: "todo_api",
     },
 );
